7086d87b07116fc240fcf2dcdca1d1d4b9f543ce
William <william.ma9215@gmail.com>
2018-11-03 00:54:21 -0400

    Save 002

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -5,5 +5,21 @@ module.exports = {
     database.Book.find(req.query)
     .then(dbModel => res.json(dbModel))
     .catch(err => res.status(422).json(err));
+  },
+  create: function(req, res) {
+    database.Book.create(req.body)
+    .then(dbModel => res.json(dbModel))
+    .catch(err => res.status(422).json(err))
+  },
+  update: function(req, res) {
+    database.Book.findOneAndUpdate({ _id: req.params.id }, req.body)
+    .then(dbModel => res.json(dbModel))
+    .catch(err => res.status(422).json(err));
+  },
+  remove: function(req, res) {
+    database.Book.findById({ _id: req.params.id })
+    .then(dbModel => dbModel.remove())
+    .then(dbModel => res.json(dbModel))
+    .catch(err => res.status(422).json(err));
   }
 };
