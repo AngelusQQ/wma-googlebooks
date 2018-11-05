95635c8375d31518d170a8b0d38eef43bc83f29c
William <william.ma9215@gmail.com>
2018-11-05 14:51:31 -0500

    Working on Linking with Google Books API?

--- client/src/pages/Books.js
+++ client/src/pages/Books.js
@@ -4,7 +4,7 @@ import API from "../utils/https";
 class Books extends Component {
   state = {
     books: [],
-    whatever: ""
+    search: ""
   };
 
   componentDidMount() {
@@ -12,16 +12,36 @@ class Books extends Component {
   }
 
   loadBooks = () => {
-    // API.getBooks('/api/books')
+    API.getBooks('/api/books')
+      .then(response => console.log(response))
+      .catch((err) => console.log(err));
+  };
+
+  saveBooks = () => {
     API.saveBook()
       .then(response => console.log(response))
       .catch((err) => console.log(err));
+  }
+
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+
+  submitSearch = event => {
+    API.searchBooks(this.state.search)
+      .then(response => console.log(typeof response))
+      .catch((err) => console.log(err))
   };
 
   render() {
     return (
       <div>
-        {this.state.whatever}
+        <form>
+          <input value={this.state.search} onChange={this.handleInputChange} name="search">
+          <button disabled={!(this.state.search)} onClick={this.submitSearch}></button>
+        </form>
+
       </div>
     )
   }
--- client/src/utils/https.js
+++ client/src/utils/https.js
@@ -9,13 +9,30 @@ const postOptions = {
           'Content-Type': 'application/x-www-form-urlencoded'
   }
 }
-
-const postData = querystring.stringify({
-  "title": "Catcher in the Rye",
-  "authors": "J. D. Salinger"
-})
+//
+// const postData = querystring.stringify({
+//   "title": "Catcher in the Rye",
+//   "authors": "J. D. Salinger"
+// })
 
 export default {
+  //GET REQUEST (Google Search - Books)
+  //https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes
+  searchBooks: (parameters) => {
+    const query = parameters.split(" ").join("+");
+    const url = 'https://www.googleapis.com/books/v1/volumes?q=' + query;
+    return new Promise((resolve, reject) => {
+      const request = https.get(url, (response) => {
+        if(response.statusCode >= 200 && response.statusCode < 300) {
+          response.on('data', (chunk) => resolve(chunk));
+        } else {
+          reject(new Error(response.statusCode));
+        }
+      })
+      request.on('error', (err) => reject(err));
+    });
+  },
+  //GET REQUEST (loading ALL books from the database)
   getBooks: (url) => {
     return new Promise((resolve, reject) => {
       const request = https.get(url, (response) => {
@@ -30,7 +47,14 @@ export default {
       request.on('error', (err) => reject(err));
     });
   },
-  saveBook: () => {
+  //POST REQUEST (adding a book to database)
+  saveBook: (title, authors, description) => {
+    const postData = querystring.stringify({
+      "title": title || "No Title Provided",
+      "authors": authors || "No Author Provided"
+      "description": description || "No Description Provided"
+    });
+
     return new Promise((resolve, reject) => {
       const request = https.request(postOptions, (response) => {
         let data = '';
@@ -46,4 +70,5 @@ export default {
       request.on('error', (err) => reject(err));
     });
   }
+  //END OF EXPORT
 }
