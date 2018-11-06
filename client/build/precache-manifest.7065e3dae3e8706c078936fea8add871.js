6416ea84e713e94a3ac6dc2bd3e18b5641c83d69
William <william.ma9215@gmail.com>
2018-11-06 04:17:37 -0500

    View Buttons

---
+++ client/build/precache-manifest.7065e3dae3e8706c078936fea8add871.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ed1574cc4fa072e240e1af4b0eccd7b6",
+    "url": "/static/media/title.ed1574cc.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e59509c346d176dc6efa",
+    "url": "/static/js/main.e59509c3.chunk.js"
+  },
+  {
+    "revision": "9f74501a81f70a83ed6a",
+    "url": "/static/js/1.9f74501a.chunk.js"
+  },
+  {
+    "revision": "e59509c346d176dc6efa",
+    "url": "/static/css/main.56fa6ce4.chunk.css"
+  },
+  {
+    "revision": "d42f9f5a9d3ae25899529858a82fccb5",
+    "url": "/index.html"
+  }
+];
