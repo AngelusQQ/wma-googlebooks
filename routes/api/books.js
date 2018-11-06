0a28cfcd03ef78f50213d1cd43ef5a72267cbe7b
William <william.ma9215@gmail.com>
2018-11-06 03:29:37 -0500

    Fixing Saved Books - Delete Feature

--- routes/api/books.js
+++ routes/api/books.js
@@ -4,6 +4,5 @@ const booksController = require("../../controllers/bookController");
 // Matches with "/api/books"
 router.route("/").get(booksController.findAll).post(booksController.create)
 
-router.route("/delete").post(booksController.delete)
-
+router.route("/delete").post(booksController.remove)
 module.exports = router;
